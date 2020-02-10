3c8c863f8939866c7933394aeb8ccf1356fb2181
Alex Keivahn Smith <Alex@Alexs-MBP-3.frontier.com>
2020-02-09 22:49:41 -0500

    adding google analytics

--- static/js/appendtemplate.js
+++ static/js/appendtemplate.js
@@ -1,6 +1,15 @@
-// basic javascript file to that adds the navbar to the header, the footer information
+// basic javascript file to add universals to all HTML pages
 
 $(function(){
+  // add the footer and header
   $("header").load("static/nav.html");
   $("footer").load("static/footer.html");
+
+  // add the Google Analytics tracker
+  // get and store the analytics data
+  $.get("static/googleAnalytics.html", function(analytics) {
+    // analystics contains the code necessary for Google Analytics tracking
+    // add that code to the beginning of head
+    $("head").prepend(analytics);
+  }, 'html');
 });
